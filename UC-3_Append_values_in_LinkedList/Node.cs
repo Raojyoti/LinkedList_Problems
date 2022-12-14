a6dd79498ba01a47cf4490743768a3bd391604fc
Raojyoti <117572975+RaoJyoti123@users.noreply.github.com>
2022-12-14 19:29:19 +0530

    Appending values in linkedlist

---
+++ UC-3_Append_values_in_LinkedList/Node.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UC_3_Append_values_in_LinkedList
+{
+    public class Node
+    {
+        public int data;
+        public Node next;
+        public Node(int data)
+        {
+            this.data = data;
+        }
+    }
+}
---
+++ UC-3_Append_values_in_LinkedList/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace UC_3_Append_values_in_LinkedList
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList linkedList = new LinkedList();
+            Console.WriteLine("Add values in linklist are: ");
+            Console.WriteLine("-----------------------------");
+            linkedList.Add(56);
+            linkedList.Add(30);
+            linkedList.Add(70);
+            Console.Write("\n");
+            Console.WriteLine("LinkedList Sequence:");
+            Console.WriteLine("-----------------------------");
+            linkedList.Display();
+            Console.WriteLine("\n");
+        }
+    }
+}
