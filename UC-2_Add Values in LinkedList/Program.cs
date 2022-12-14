74fb50df380c3c4c24d16f279993bd91778eef7b
Raojyoti <117572975+RaoJyoti123@users.noreply.github.com>
2022-12-14 19:22:46 +0530

    Add values in linkedlist

---
+++ UC-2_Add Values in LinkedList/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace UC_2_Add_Values_in_LinkedList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList linkedList = new LinkedList();
+            Console.WriteLine("Add values in linklist are: ");
+            Console.WriteLine("-----------------------------");
+            linkedList.Add(56);
+            linkedList.Add(30);
+            linkedList.Add(70);
+            Console.Write("\n");
+            Console.WriteLine("LinkedList Sequence:");
+            Console.WriteLine("-----------------------------");
+            linkedList.Display();
+            Console.WriteLine("\n");
+        }
+    }
+}
