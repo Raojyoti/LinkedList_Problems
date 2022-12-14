2eb5fcd781358e0151adb9093bd1cdda812a5a77
Raojyoti <117572975+RaoJyoti123@users.noreply.github.com>
2022-12-14 19:37:04 +0530

    Delete first element from linkedlist

---
+++ UC-5_Delete first element in LinkedList/Node.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UC_5_Delete_first_element_in_LinkedList
+{
+    public class Node
+    {
+        public int data;
+        public Node next;
+        public Node(int data) 
+        { 
+            this.data = data;
+        }
+    }
+}
---
+++ UC-5_Delete first element in LinkedList/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace UC_5_Delete_first_element_in_LinkedList
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList List = new LinkedList();
+            Console.WriteLine("Add values in linklist are: ");
+            Console.WriteLine("--------------------------------");
+            List.Add(56);
+            List.Add(30);
+            List.Add(70);
+            Console.Write("\n");
+            Console.WriteLine("LinkedList Sequence:");
+            Console.WriteLine("-----------------------------");
+            List.Display();
+            Console.WriteLine("\n");
+
+            Console.WriteLine("After delete the first value and the LinkedList sequence are:");
+            Console.WriteLine("------------------------------------------------------------------");
+            List.RemoveFirstNode();
+            List.Display();
+            Console.WriteLine("\n");
+           
+        }
+    }
+}
