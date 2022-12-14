f98bba1a7baec130d56d509aa30730a2c8e2cb8a
Raojyoti <117572975+RaoJyoti123@users.noreply.github.com>
2022-12-14 19:33:02 +0530

    Insert value in between values

---
+++ UC-4_Insert_Values_Between/LinkedList.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UC_4_Insert_Values_Between
+{
+    public class LinkedList
+    {
+        public Node head;
+        public void Add(int data)
+        {
+            Node node = new Node(data);
+            if (this.head == null)
+            {
+                this.head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine("{0} is inserted in linked list", node.data);
+        }
+        public void Display()
+        {
+            Node temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("LinkedList is empty");
+                return;
+            }
+            while (temp != null)
+            {
+                Console.Write(temp.data + "->");
+                temp = temp.next;
+            }
+        }
+        public int Search(int value)
+        {
+            Node node =this.head;
+            int count = 1;
+            while (node != null)
+            {
+                if(node.data == value)
+                {
+                    return count;
+                }
+                node= node.next;
+                count++;
+            }
+            return count;
+        }
+        public Node InsertAtParticularPosition(int position,int data) 
+        {
+            if (position < 1)
+                Console.WriteLine("Invalid position");
+            if(position == 1)
+            {
+                var newNode= new Node(data);
+                newNode.next = this.head;
+                head = newNode;
+            }
+            else
+            {
+                while(position-- !=0)
+                {
+                    if(position == 1)
+                    {
+                        Node node =new Node(data);
+                        node.next = this.head.next;
+                        head.next = node;
+                        break;
+                    }
+                    head = head.next;
+                }
+                if(position !=1)
+                    Console.WriteLine("Position is out of range");
+            }
+            return head;
+        }
+    }
+}
---
+++ UC-4_Insert_Values_Between/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace UC_4_Insert_Values_Between
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList List = new LinkedList();
+            Console.WriteLine("Add values in linklist are: ");
+            Console.WriteLine("-----------------------------");
+            List.Add(56);
+            List.Add(70);
+            Console.Write("\n");
+            Console.WriteLine("LinkedList Sequence:");
+            Console.WriteLine("-----------------------------");
+            List.Display();
+            int a = List.Search(56);
+            Console.WriteLine("Index position of element 56 is: " + a);
+            List.InsertAtParticularPosition(a+1, 30);
+            Console.WriteLine("-----------------------------");
+            List.Display();
+            Console.WriteLine("\n");
+            
+        }
+    }
+}
