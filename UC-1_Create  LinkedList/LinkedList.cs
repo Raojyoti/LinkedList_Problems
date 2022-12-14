dd2797e78ad8ae251836ff3e46d16795a9094331
Raojyoti <117572975+RaoJyoti123@users.noreply.github.com>
2022-12-14 19:15:12 +0530

    create linkedlist

---
+++ UC-1_Create  LinkedList/LinkedList.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UC_1_Create__LinkedList
+{
+    public class LinkedList
+    {
+        public Node head;
+        public void Add(int data)
+        {
+            Node node = new Node(data);
+            if (this.head == null)
+            {
+                this.head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine("{0} is inserted in linked list", node.data);
+        }
+    }
+}
---
+++ UC-1_Create  LinkedList/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace UC_1_Create__LinkedList
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList linkedList = new LinkedList();
+            linkedList.Add(56);
+            linkedList.Add(30);
+            linkedList.Add(70);
+        }
+    }
+}
